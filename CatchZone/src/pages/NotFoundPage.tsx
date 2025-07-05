3c6007a054a210bfd9ed10fb5d216a1461956a43
Witek <wiktorskid@wp.pl>
2025-07-05 21:29:34 +0200

    Add: Not found page

--- CatchZone/src/pages/NotFoundPage.tsx
+++ CatchZone/src/pages/NotFoundPage.tsx
@@ -1,10 +1,23 @@
 import {Link} from "react-router-dom";
+import SadPikachu from "../assets/images/PikachuSad.webp";
 
 const NotFoundPage = () => {
   return (
-    <button>
-      <Link to='/'>Powrót</Link>
-    </button>
+    <div className='flex flex-col w-full h-screen  justify-center items-center'>
+      <img src={SadPikachu} className='w-[400px] h-[400px]' />
+
+      <div className='flex flex-col items-start text-justify   '>
+        <span className='text-4xl font-bold w-full'>Oops! Something went wrong.</span>
+        <span className='text-[27px] text-gray-400 w-full'>We can't find the page you're looking for.</span>
+      </div>
+
+      <Link
+        to='/'
+        className='text-2xl px-12 py-2 bg-[var(--color-primary)] text-white rounded-md mt-6 hover:scale-105 duration-300 cursor-ponter'
+      >
+        Back
+      </Link>
+    </div>
   );
 };
 
